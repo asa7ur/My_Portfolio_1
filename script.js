62dd8ae31b61408a0e22854445735128cbb0b92e
Garik Asatryan <asa7ur@gmail.com>
2024-03-20 11:34:54 +0100

    Some minor changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-emailjs.init('USER_ID') // Replace with your EmailJS User ID
+emailjs.init('PUBLIC_KEY') // Replace with your EmailJS User ID
 
 const sendBtn = document.querySelector('.send-btn')
 const result = document.querySelector('.result')
@@ -7,16 +7,16 @@ sendBtn.addEventListener('click', sendEmail)
 
 function sendEmail() {
 	// Get the form data
-	const from_name = document.getElementById('name').value
-	const from_email = document.getElementById('email').value
+	const user_name = document.getElementById('user_name').value
+	const user_email = document.getElementById('user_email').value
 	const subject = document.getElementById('subject').value
 	const message = document.getElementById('message').value
 
 	// Send the email using EmailJS
 	emailjs
 		.send('SERVICE_ID', 'TEMPLATE_ID', {
-			from_name: from_name,
-			from_email: from_email,
+			user_name: user_name,
+			user_email: user_email,
 			subject: subject,
 			message: message,
 		})
